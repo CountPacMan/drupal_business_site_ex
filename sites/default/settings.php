4d7445262a5fb3e4c12a463089ce8ab23bf10b02
ckw <cynthia.k.wilhelm@gmail.com>
2015-05-27 16:44:21 -0700

    add zen subtheme

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'coffee_ex',
       'username' => 'pimby',
       'password' => 'password',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
